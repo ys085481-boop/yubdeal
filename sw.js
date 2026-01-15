9e82ead74ed77ccf9ff65d22e39c35a43f315008
yubdeal <ys085481@gmail.com>
2026-01-15 21:01:19 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10463791
+    "domain": "5gvci.com",
+    "zoneId": 10468533
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
